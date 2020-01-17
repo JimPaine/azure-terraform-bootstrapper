8178d57193310e7709ab451b781dcd49685b5d85
Jim Paine <jim@jimpaine.io>
2020-01-17 23:45:39 +0000

    updated providers and syntax

--- bootsrapper/main.tf
+++ bootsrapper/main.tf
@@ -1,13 +1,13 @@
 provider "azuread" {
-  version = "~> 0.4"
+  version = "~> 0.7"
 }
 
 provider "azurerm" {
-  version = "~> 1.30.1"
+  version = "~> 1.41"
 }
 
 provider "random" {
-  version = "~> 2.1"
+  version = "~> 2.2"
 }
 
 provider "external" {
@@ -26,13 +26,13 @@ variable "resource_group_location" {
 
 
 resource "azurerm_resource_group" "lab" {
-  name     = "${var.resource_group_name}"
-  location = "${var.resource_group_location}"    
+  name     = var.resource_group_name
+  location = var.resource_group_location    
 }
 
 resource "random_id" "lab" {
   keepers = {
-    resource_group = "${azurerm_resource_group.lab.name}"
+    resource_group = "azurerm_resource_group.lab.name"
   }
 
   byte_length = 2
@@ -48,7 +48,7 @@ resource "azuread_application" "lab" {
 }
 
 resource "azuread_service_principal" "lab" {
-  application_id = "${azuread_application.lab.application_id}"
+  application_id = azuread_application.lab.application_id
 }
 
 resource "random_string" "lab" {
@@ -57,8 +57,8 @@ resource "random_string" "lab" {
 }
 
 resource "azuread_service_principal_password" "lab" {
-  service_principal_id = "${azuread_service_principal.lab.id}"
-  value                = "${random_string.lab.result}"
+  service_principal_id = azuread_service_principal.lab.id
+  value                = random_string.lab.result
   end_date             = "2020-01-01T01:02:03Z"
 }
 
@@ -72,22 +72,22 @@ data "external" "lab" {
 resource "azurerm_role_assignment" "lab" {
   scope                = "/subscriptions/${data.azurerm_client_config.lab.subscription_id}"
   role_definition_name = "Owner"
-  principal_id         = "${azuread_service_principal.lab.id}"
+  principal_id         = azuread_service_principal.lab.id
 }
 
 resource "azurerm_key_vault" "lab" {
   name                = "vault${random_id.lab.dec}"
-  location            = "${azurerm_resource_group.lab.location}"
-  resource_group_name = "${azurerm_resource_group.lab.name}"
-  tenant_id           = "${data.azurerm_client_config.lab.tenant_id}"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  tenant_id           = data.azurerm_client_config.lab.tenant_id
 
   sku {
     name = "standard"
   }
 
   access_policy {
-    tenant_id = "${data.azurerm_client_config.lab.tenant_id}"
-    object_id = "${data.external.lab.result.objectId}"
+    tenant_id = data.azurerm_client_config.lab.tenant_id
+    object_id = data.external.lab.result.objectId
 
     key_permissions = []
 
@@ -102,64 +102,64 @@ resource "azurerm_key_vault" "lab" {
 
 resource "azurerm_key_vault_secret" "client-id" {
     name = "clientid"
-    value = "${azuread_application.lab.application_id}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = azuread_application.lab.application_id
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_key_vault_secret" "client-secret" {
     name = "clientsecret"
-    value = "${random_string.lab.result}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = random_string.lab.result
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_key_vault_secret" "subscription-id" {
     name = "subscriptionid"
-    value = "${data.azurerm_client_config.lab.subscription_id}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = data.azurerm_client_config.lab.subscription_id
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_key_vault_secret" "tenant-id" {
     name = "tenantid"
-    value = "${data.azurerm_client_config.lab.tenant_id}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = data.azurerm_client_config.lab.tenant_id
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_storage_account" "lab" {
   name                     = "terraformstate${random_id.lab.dec}"
-  resource_group_name      = "${azurerm_resource_group.lab.name}"
-  location                 = "${azurerm_resource_group.lab.location}"
+  resource_group_name      = azurerm_resource_group.lab.name
+  location                 = azurerm_resource_group.lab.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "lab" {
   name                  = "state"
-  resource_group_name   = "${azurerm_resource_group.lab.name}"
-  storage_account_name  = "${azurerm_storage_account.lab.name}"
+  resource_group_name   = azurerm_resource_group.lab.name
+  storage_account_name  = azurerm_storage_account.lab.name
   container_access_type = "private"
 }
 
 resource "azurerm_key_vault_secret" "storage-account" {
     name = "storageaccount"
-    value = "${azurerm_storage_account.lab.name}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = azurerm_storage_account.lab.name
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_key_vault_secret" "container-name" {
     name = "containername"
-    value = "${azurerm_storage_container.lab.name}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = azurerm_storage_container.lab.name
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 resource "azurerm_key_vault_secret" "access-key" {
     name = "accesskey"
-    value = "${azurerm_storage_account.lab.primary_access_key}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = azurerm_storage_account.lab.primary_access_key
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
 
 
 resource "azurerm_key_vault_secret" "resource-group" {
     name = "resourcegroup"
-    value = "${azurerm_resource_group.lab.name}"
-    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+    value = azurerm_resource_group.lab.name
+    vault_uri = azurerm_key_vault.lab.vault_uri
 }
